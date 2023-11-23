162a4bce897226d55d8588aa2e6ebb8eed557759
sfmk07 <151550819+sfmk07@users.noreply.github.com>
2023-11-23 15:09:40 +0100

    Update exercice7.sh

--- exercice7.sh
+++ exercice7.sh
@@ -8,11 +8,11 @@ read -p "Voulez-vous créer le fichier $nom_fichier ? (oui/non) : " confirmation
 
 
 if [ "$confirmation" == "oui" ]; then
-    # Créer le fichier
+    
     touch "$nom_fichier"
     echo "Le fichier $nom_fichier a été créé."
 else
-    # Afficher un message de sortie
+    
     echo "Opération annulée. Aucun fichier n'a été créé."
 fi
 
