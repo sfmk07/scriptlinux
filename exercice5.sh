268d19f7bd86005c4046a82f2b180b113276b95a
sfmk07 <151550819+sfmk07@users.noreply.github.com>
2023-11-23 10:00:16 +0100

    exercice5

---
+++ exercice5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Entrez le premier nombre : " nombre1
+
+
+read -p "Entrez le deuxième nombre : " nombre2
+
+
+somme=$((nombre1 + nombre2))
+
+
+echo "La somme de $nombre1 et $nombre2 est : $somme"
+
